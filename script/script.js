5e9e2021264ed093258ad16ae8b40e905f52a0a1
jordyf15 <jordyferdian88@gmail.com>
2021-08-21 09:09:45 +0700

    created operate function

--- script/script.js
+++ script/script.js
@@ -13,3 +13,16 @@ function multiply(aNum, bNum){
 function divide(aNum, bNum){
     return aNum/bNum;
 }
+
+function operate(operator, aNum, bNum){
+    switch(operator){
+        case '+':
+            return add(aNum, bNum);
+        case '-':
+            return subtract(aNum, bNum);
+        case '*':
+            return multiply(aNum, bNum);
+        case '/':
+            return divide(aNum, bNum);    
+    }
+}
