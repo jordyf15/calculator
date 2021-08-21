3e95283ca5e6bdcb389e32fa01001b9d18d6dc4a
jordyf15 <jordyferdian88@gmail.com>
2021-08-21 08:47:15 +0700

    created basic math operators

---
+++ script/script.js
@@ -0,0 +1,15 @@
+function add(aNum, bNum){
+    return aNum+bNum;
+}
+
+function subtract(aNum, bNum){
+    return aNum-bNum;
+}
+
+function multiply(aNum, bNum){
+    return aNum*bNum;
+}
+
+function divide(aNum, bNum){
+    return aNum/bNum;
+}
